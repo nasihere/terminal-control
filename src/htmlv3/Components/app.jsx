ea6af3ec16871eb853b62f75a579a0f157895e86
wallacen <noah.wallace@adp.com>
2017-03-16 19:45:15 -0400

    installed webpack and react. have a working simple model

---
+++ src/htmlv3/Components/app.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import {NavBarInstance} from './NavBar/index.jsx'
+
+export const App = (props) => (
+    <div>
+    <NavBarInstance/>
+    </div>
+);
---
+++ src/htmlv3/app.js
@@ -0,0 +1,5 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {App} from './Components/app.jsx'
+import styles from "./css/bootstrap.darkly.min.css"
+ReactDOM.render( <App/> , document.getElementById("app"));
