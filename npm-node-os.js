d9b435a4bed3361fd1c63d9eeb7744d4756c5313
Nasir Sayed <nasir.sayed@adp.com>
2017-03-17 08:55:54 -0400

    s q added in clean command

--- npm-node-os.js
+++ npm-node-os.js
@@ -5,7 +5,7 @@ let npm = require("child_process"),
 
 let winCmds = {
     copy: "robocopy src\\htmlv2 build\\htmlv2 /e",
-    clean:"rmdir build /s"
+    clean:"rmdir build /s/q"
 };
 let linCmds = {
     copy:"cp -R ./src/htmlv2 ./build/htmlv2;",
