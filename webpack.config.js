d2fa54c915dc1215fbe16510e07469ceb96bf33c
wallacen <noah.wallace@adp.com>
2017-03-20 19:29:55 -0400

    cleaned up package.json. removed webpack-dashboard it is redundant. added build and prebuild scripts. now package will need to be run seperately

--- webpack.config.js
+++ webpack.config.js
@@ -50,6 +50,52 @@ module.exports = {
         new ExtractTextPlugin('./css/bootstrap.dark.min.css'),
         new ExtractTextPlugin('./css/base.css'),
         new HtmlWebpackPlugin({title: 'Tree-Shaking', template: "./src/htmlv3/index.html"})
-    ]
+    ],
+    devtool:"inline-source-map",
+    stats:{// Add asset Information
+        assets: true,
+        // Sort assets by a field
+        assetsSort: "field",
+        // Add information about cached (not built) modules
+        cached: false,
+        // Add children information
+        children: false,
+        // Add chunk information (setting this to `false` allows for a less verbose output)
+        // there is a bug where this is ignored 2017-3-21
+        // https://github.com/webpack/webpack/issues/4230
+        chunks: false,
+        // Add built modules information to chunk information
+        chunkModules: false,
+        // Add the origins of chunks and chunk merging info
+        chunkOrigins: false,
+        // Sort the chunks by a field
+        chunksSort: "field",
+        // Context directory for request shortening
+        //context: "./src/",
+        // `webpack --colors` equivalent
+        colors: true,
+        // Add errors
+        errors: true,
+        // Add details to errors (like resolving log)
+        errorDetails: true,
+        // Add the hash of the compilation
+        hash: true,
+        // Add built modules information
+        modules: false,
+        // Sort the modules by a field
+        modulesSort: "field",
+        // Add public path information
+        publicPath: false,
+        // Add information about the reasons why modules are included
+        reasons: false,
+        // Add the source code of modules
+        source: false,
+        // Add timing information
+        timings: true,
+        // Add webpack version information
+        version: true,
+        // Add warnings
+        warnings: true
+    }
 
 }
