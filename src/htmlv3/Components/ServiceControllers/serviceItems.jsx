6e108be3dcce5c2455af8cbf886ef98277003262
wallacen <noah.wallace@adp.com>
2017-03-28 17:18:25 -0400

    more css changes

--- src/htmlv3/Components/ServiceControllers/serviceItems.jsx
+++ src/htmlv3/Components/ServiceControllers/serviceItems.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import {ListGroupItem, Button, Glyphicon, Col} from 'react-bootstrap';
+import {ListGroupItem, Button, Glyphicon, ButtonGroup} from 'react-bootstrap';
 import './serviceItems.css';
 export class ServiceItems extends React.Component {
     getAction = (item) => {
         let action;
         !item.connected ? action= (
-            <div className="btn btn-sm" onClick={() => this.props.startService(item)}>
+            <div className="btn" onClick={() => this.props.startService(item)}>
                 <Glyphicon glyph="play"/>
             </div>): action=
-        (<div className="btn btn-sm" onClick={() => this.props.killService(item)}>
+        (<div className="btn" onClick={() => this.props.killService(item)}>
             <Glyphicon glyph="stop"/>
         </div>)
         return action;
@@ -25,19 +25,21 @@ export class ServiceItems extends React.Component {
                     return (
                         <ListGroupItem key={idx.toString()} className="service-group">
 
-                            {this.getAction(item)}
+
 
                             {item.name}
-                            <div className="pull-right">
-
+                            <ButtonGroup bsStyle="small pull-right">
+                                {this.getAction(item)}
                            {/* <div className="btn" onClick={() => this.props.pingService(item)}>Ping</div>*/}
-                            <div className="btn btn-sm" onClick={() => this.props.openConfigModal(item,'delete')}>
-                                <Glyphicon glyph="remove-sign"/>
-                            </div>
-                            <div className="btn btn-sm" onClick={() => this.props.openConfigModal(item,'edit')}>
+
+                            <div className="btn" onClick={() => this.props.openConfigModal(item,'edit')}>
                                 <Glyphicon glyph="edit"/>
                             </div>
+                            <div className="btn" onClick={() => this.props.openConfigModal(item,'delete')}>
+                                <Glyphicon glyph="remove-sign"/>
                             </div>
+
+                            </ButtonGroup>
                             {errorMsg}
                         </ListGroupItem>
                     )
@@ -45,7 +47,7 @@ export class ServiceItems extends React.Component {
         return (
             <div>
                 <ListGroupItem key={"btn#0"}>
-                <Button type="button" onClick={() => this.props.openConfigModal(null, 'new')}>Add New
+                <Button type="button" bsSize="small" className="pull-right" onClick={() => this.props.openConfigModal(null, 'new')}>Add New
                     Service</Button>
                 </ListGroupItem>
                 {services}</div>)
