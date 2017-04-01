f1d57bd1b875c43b00adcb727557ebe4997fff7b
Nasir Sayed <nasir.sayed@adp.com>
2017-04-01 16:33:56 -0400

    Redux Page Removed

--- src/htmlv3/Framework/app.jsx
+++ src/htmlv3/Framework/app.jsx
@@ -18,7 +18,7 @@ export class AppClass extends React.Component {
     render () {
         return (
             <div className="container-fluid">
-                <ReduxTest />
+                {/*<ReduxTest />*/}
                 <Layout />
                 {/*<TemplateDemo />*/}
                 {/*<NavBarInstance />*/}
